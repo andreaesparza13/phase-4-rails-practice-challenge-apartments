ed889c59b1299744a06671f1dc62cccce111591e
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-09-08 14:24:58 -0700

    done

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,12 +1,12 @@
 class TenantsController < ApplicationController
 
    def index
-      render json: Tenant.all, status: :ok
+      render json: Tenant.all
    end
 
    def show
       tenant = Tenant.find(params[:id])
-      render json: tenant, status: :ok
+      render json: tenant
    end
 
    def create
